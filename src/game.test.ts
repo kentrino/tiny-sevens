784b382b41ed996a00c364ec72f6e0d41a4236f1
kentrino <kentrino@h-kento.jp>
2024-03-27 22:11:29 +0900

    fix test for parse

--- src/game.test.ts
+++ src/game.test.ts
@@ -3,15 +3,17 @@ import { random } from './random.ts'
 import { type Action, initialGame, run } from './game.ts'
 import { format } from './helpers/format.ts'
 import { apply } from './helpers/apply.ts'
+import { parse } from "./helpers/parse.ts"
 
 describe('game', () => {
-  function initializeRandomWithSeed() {
+
+  function initGame() {
     random('test seed')
+    return initialGame(4)
   }
 
   it('format', () => {
-    initializeRandomWithSeed()
-    const game = initialGame(4)
+    const game = initGame()
     expect(format(game)).toEqual({
       currentPlayer: 0,
       field: {
@@ -33,9 +35,31 @@ describe('game', () => {
     })
   })
 
+  it('parse', () => {
+    const game = initGame()
+    expect(parse({
+      currentPlayer: 0,
+      field: {
+        C: '..............',
+        D: '..............',
+        H: '..............',
+        S: '..............',
+      },
+      hands: [
+        'SK S2 CK D7 C0 S9 D0 S8 HJ CA HK H5 SQ',
+        'SA DK H4 D8 D9 S0 SJ C5 HQ H0 HA H2 D5',
+        'H9 C9 DJ DA CQ CJ H6 DQ S3 C3 D2 C8 D3',
+        'C2 C4 H3 S4 H7 S6 S7 D4 C7 H8 D6 S5 C6',
+      ],
+      losers: [],
+      numPlayers: 4,
+      skips: [0, 0, 0, 0],
+      turn: 0,
+    })).toEqual(game)
+  })
+
   it('after initial action', () => {
-    initializeRandomWithSeed()
-    const game = initialGame(4)
+    const game = initGame()
     const { game: newGame } = run(game, { type: 'initial' })
     expect(format(newGame)).toEqual({
       currentPlayer: 0,
@@ -59,8 +83,7 @@ describe('game', () => {
   })
 
   it('after initial action and first move', () => {
-    initializeRandomWithSeed()
-    const startGame = initialGame(4)
+    const startGame = initGame()
     const actions: Action[] = [
       { type: 'initial' },
       { type: 'card', card: { number: '8', suit: 'S' }, player: 0 },
@@ -87,8 +110,7 @@ describe('game', () => {
     })
   })
   it('many skips', () => {
-    initializeRandomWithSeed()
-    const startGame = initialGame(4)
+    const startGame = initGame()
     const actions: Action[] = [
       { type: 'initial' },
       { type: 'card', card: { number: '8', suit: 'S' }, player: 0 },
--- src/helpers/mapValues.ts
+++ src/helpers/mapValues.ts
@@ -1,10 +1,10 @@
 export function mapValues<T extends Record<string, unknown>, S>(
   obj: T,
-  fn: (value: T[keyof T]) => S,
+  fn: (value: T[keyof T], key: string) => S,
 ): { [K in keyof T]: S } {
   const result: { [K in keyof T]: S } = {} as never
   for (const key in obj) {
-    result[key] = fn(obj[key])
+    result[key] = fn(obj[key], key)
   }
   return result
 }
